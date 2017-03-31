60e2b404b0a195316873c839cdac9ae9fbe91b29
David Barth <davidbarth80@gmail.com>
2017-03-31 16:59:08 +0100

    refactor using expressions to replace magic strings

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,7 +1,10 @@
-﻿using GigHub.Models;
+﻿using GigHub.Controllers;
+using GigHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace GigHub.ViewModels
 {
@@ -32,7 +35,37 @@ namespace GigHub.ViewModels
         //if Id is not 0 call Update action of Gigs contoller othwerwise Create action
         //the Id will be rendered as a hidden field so when we post the form we know which
         //gig we are modifying see Gigform for hidden field
+        /*
         public string Action => (Id != 0) ? "Update" : "Create";
+        */
+
+        public string Action
+        {
+            get
+            {   //going from step 1 to the new solution
+                // 1. return Id != 0 ? "Update" : "Create"; //magic strings - to fragile because 
+                //when you rename any of the methods the code will break
+
+                //2. var update = (c => c.Update()); //anyonmus method can be represented with a Func delegate
+
+                //3. c represent GigsController therefor the arguments in the delegate are
+                //the GigsController and return type of its method Update
+                //Func<GigsController,ActionResult> update = (c => c.Update(this));
+                //Func<GigsController, ActionResult> create = (c => c.Create(this));
+
+                //wrapping the delegate with expression because we don't want to call the delegate 
+                //just representing the methods via an expression
+                Expression<Func<GigsController,ActionResult>> update = (c => c.Update(this));
+                Expression<Func<GigsController, ActionResult>> create = (c => c.Create(this));
+
+                //determining action with this expression
+                var action = (Id != 0) ? update : create;
+
+                return (action.Body as MethodCallExpression).Method.Name;
+               
+            }
+
+        } 
 
         public DateTime GetDateTime()
         {
