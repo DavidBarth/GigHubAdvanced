691954a0bb9f731ede86636f7fc4aa43bbebb188
David Barth <davidbarth80@gmail.com>
2017-04-01 22:53:54 +0100

    Consuming API with JQuery Ajax

--- GigHub/Migrations/Configuration.cs
+++ GigHub/Migrations/Configuration.cs
@@ -1,15 +1,12 @@
 namespace GigHub.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<GigHub.Models.ApplicationDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(GigHub.Models.ApplicationDbContext context)
