3b8c66e7f59de89e72367f7cb1fe5beb88db686d
David Barth <davidbarth80@gmail.com>
2017-04-29 00:23:18 +0100

    Initialize AutoMap profile in Global.asax.cs

---
+++ GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GigHub.Controllers.Api;
+using GigHub.Models;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap generic type takes a source and destination param
+            Mapper.CreateMap<ApplicationUser, UserDto>();
+            Mapper.CreateMap<Gig, GigDto>();
+            Mapper.CreateMap<Notification, NotificationDto>();
+        }
+    }
+}
